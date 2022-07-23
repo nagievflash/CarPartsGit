78586623cef1655a08ef0082f91038c748a46af8
Tarlan Nagiev <tarlan.nagiev@mail.ru>
2022-07-23 16:52:30 +0300

    Add Product model and migration

---
+++ database/migrations/2022_07_23_133709_create_products_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('title');
+            $table->string('SKU')->unique();
+            $table->decimal('price', $precision = 8, $scale = 2)->nullable();
+            $table->text('desc')->nullable();
+            $table->unsignedBigInteger('category_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
