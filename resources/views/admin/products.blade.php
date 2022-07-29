821b13ce38aca5f7ad70598a055073380155b479
Tarlan Nagiev <tarlan.nagiev@mail.ru>
2022-07-29 15:26:09 +0300

    Changed products template

--- resources/views/admin/products.blade.php
+++ resources/views/admin/products.blade.php
@@ -24,25 +24,50 @@
                     <table class="table table-striped" id="table1">
                         <thead>
                         <tr>
+                            <th></th>
                             <th>Image</th>
                             <th>SKU</th>
+                            <th>Title</th>
                             <th>Qty</th>
                             <th>Price</th>
                             <th>Status</th>
+                            <th>Actions</th>
                         </tr>
                         </thead>
                         <tbody>
+                        @foreach ($products as $product)
                         <tr>
-                            <td>Graiden</td>
-                            <td>vehicula.aliquet@semconsequat.co.uk</td>
-                            <td>076 4820 8838</td>
-                            <td>Offenburg</td>
+                            @php
+                            $image = explode(',', $product->images)[0];
+                            @endphp
+
+                            <td style="text-align: center;">
+                                <div class="form-check">
+                                    <div class="checkbox">
+                                        <input type="checkbox" class="form-check-input" id="{{$product->sku}}" name="{{$product->sku}}">
+                                    </div>
+                                </div>
+                            </td>
+                            <td style=""><img src="{{$image}}" alt="{{$product->title}}" width="75px" /></td>
+                            <td>{{$product->sku}}</td>
+                            <td>{{$product->title}}</td>
+                            <td>{{$product->qty}}</td>
+                            <td>{{$product->price}}</td>
                             <td>
-                                <span class="badge bg-success">Active</span>
+                                <span class="badge bg-danger">Inactive</span>
+                            </td>
+                            <td>
+                                <a href="/admin/products/{{$product->id}}/edit" class="text-primary h4 p-1" title="Edit product"><i class="bi bi-pen"></i></a>
+                                <a href="/admin/products/{{$product->id}}/delete" class="text-danger h4 p-1" title="Delete product"><i class="bi bi-trash"></i></a>
+                                <a href="/admin/ebay/upload?sku={{$product->sku}}" class="text-success h4 p-1" title="Upload product to Ebay"><i class="bi bi-cloud-upload"></i></a>
                             </td>
                         </tr>
+                        @endforeach
                         </tbody>
                     </table>
+                    <div class="dataTable-bottom">
+                        {{ $products->links() }}
+                    </div>
                 </div>
             </div>
 
