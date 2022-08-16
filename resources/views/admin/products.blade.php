676651ce1e850b7c7a2d0b2e02061339025b7747
Tarlan Nagiev <tarlan.nagiev@mail.ru>
2022-08-16 14:10:42 +0300

    Added return message to product list template Remove image if not exists

--- resources/views/admin/products.blade.php
+++ resources/views/admin/products.blade.php
@@ -24,7 +24,7 @@
                     @if (\Session::has('success'))
                         <div class="alert alert-success">
                             <ul>
-                                <li>{{ \Session::get('success') }}</li>
+                                <li>{!! \Session::get('success') !!}</li>
                             </ul>
                         </div>
                     @endif
@@ -70,18 +70,23 @@
                                     </div>
                                 </div>
                             </td>
-                            <td style=""><img src="{{$image}}" alt="{{$product->title}}" width="75px" /></td>
+                            <td style="">
+                                @if ($image)
+                                <img src="{{$image}}" alt="{{$product->title}}" width="75px" />
+                                @endif
+                            </td>
                             <td>{{$product->sku}}</td>
                             <td>{{$product->getTitle()}}</td>
                             <td>{{$product->qty}}</td>
-                            <td>{{$price = $product->price + $product->price * .3}}</td>
+                            <td>{{$price = $product->price + $product->price * .25}}</td>
                             <td>
                                 <span class="badge bg-danger">Inactive</span>
                             </td>
                             <td>
-                                <a href="/admin/products/{{$product->id}}/edit" class="text-primary h4 p-1" title="Edit product"><i class="bi bi-pen"></i></a>
-                                <a href="/admin/products/{{$product->id}}/delete" class="text-danger h4 p-1" title="Delete product"><i class="bi bi-trash"></i></a>
-                                <a href="/admin/ebay/upload?sku={{$product->sku}}" class="text-success h4 p-1" title="Upload product to Ebay"><i class="bi bi-cloud-upload"></i></a>
+                                {{--<a href="/admin/products/{{$product->id}}/edit" class="text-primary h4 p-1" title="Edit product"><i class="bi bi-pen"></i></a>
+                                <a href="/admin/products/{{$product->id}}/delete" class="text-danger h4 p-1" title="Delete product"><i class="bi bi-trash"></i></a>--}}
+                                <a href="/admin/ebay/upload?sku={{$product->sku}}" class="text-success h4 p-1" title="Upload product to Ebay"><i class="bi bi-cloud-arrow-up"></i></a>
+                                <a href="/admin/ebay/revise?sku={{$product->sku}}" class="text-success h4 p-1" title="Revise Item at Ebay"><i class="bi bi-arrow-repeat"></i></a>
                             </td>
                         </tr>
                         @endforeach
