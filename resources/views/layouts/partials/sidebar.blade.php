728adf95e29d88e0bd018d037c481ab9762096ec
Tarlan Nagiev <tarlan.nagiev@mail.ru>
2022-08-19 12:22:08 +0300

    Added some menu items

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -4,6 +4,8 @@
 
     <x-maz-sidebar-item name="Dashboard" :link="route('dashboard')" icon="bi bi-grid-fill"></x-maz-sidebar-item>
     <x-maz-sidebar-item name="Products" :link="route('products.list')" icon="bi bi-box"></x-maz-sidebar-item>
+    <x-maz-sidebar-item name="Ebay Listings" :link="route('ebay.listings')" icon="bi bi-shop"></x-maz-sidebar-item>
     <x-maz-sidebar-item name="CSV Import" :link="route('import')" icon="bi bi-box-arrow-in-down-left"></x-maz-sidebar-item>
+    <x-maz-sidebar-item name="Upload Ebay" :link="route('ebayUpload')" icon="bi bi-box-arrow-in-down-left"></x-maz-sidebar-item>
 
 </x-maz-sidebar>
