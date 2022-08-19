8879879bf426881e2b8b3572b61960f770d7466a
Tarlan Nagiev <tarlan.nagiev@mail.ru>
2022-08-19 12:11:14 +0300

    Added ebayListings and ebayUpload methods in AdminController.php

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\EbayListing;
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
@@ -10,5 +11,14 @@ class AdminController extends Controller
     public function importProductsFromCSV(Request $request) {
         $code = $request->has('code') ? $request->input('code') : '';
         return view('admin.importCSVItem')->with('code', $code);
+    }
+
+    public function ebayListings(Request $request) {
+        $listings = EbayListing::paginate(25);
+        return view('ebay.listings')->with('listings', $listings);
+    }
+
+    public function ebayUpload(Request $request) {
+        return view('admin.ebayUpload');
     }
 }
