7053899b033b1a110991ce17facf71483b7bca72
Tarlan Nagiev <tarlan.nagiev@mail.ru>
2022-08-19 12:22:20 +0300

    Added some routes to routes list

--- routes/web.php
+++ routes/web.php
@@ -25,11 +25,15 @@ Route::middleware(['auth:sanctum', 'verified'])->prefix('admin')->group(function
     })->name('dashboard');
 
     Route::get('/import',  [App\Http\Controllers\Admin\AdminController::class, 'importProductsFromCSV'])->name('import');
+    Route::get('/import/products/custom',  [App\Http\Controllers\Admin\AdminController::class, 'ebayUpload'])->name('ebayUpload');
     Route::get('/categories',  [App\Http\Controllers\Admin\CategoriesController::class, 'createCategoriesFromJson'])->name('categoriesList');
     Route::get('/products',  [App\Http\Controllers\Admin\ProductsController::class, 'index'])->name('products.list');
+    Route::get('/ebay/listings',  [App\Http\Controllers\Admin\AdminController::class, 'ebayListings'])->name('ebay.listings');
 
     Route::post('/import',  [App\Http\Controllers\Admin\RequestController::class, 'importProductsBasics']);
+    Route::post('/import/products/custom',  [App\Http\Controllers\Admin\RequestController::class, 'importProductsCustom']);
     Route::get('/ebay/upload',  [App\Http\Controllers\Admin\EbayController::class, 'addFixedPriceItem']);
+    Route::get('/ebay/revise',  [App\Http\Controllers\Admin\EbayController::class, 'reviseFixedPriceItem']);
 });
 
 
