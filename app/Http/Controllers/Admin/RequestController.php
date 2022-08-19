11c9c19228d78c95d7bb277982c27da528d07377
Tarlan Nagiev <tarlan.nagiev@mail.ru>
2022-08-19 12:21:28 +0300

    Added importProductsCustom to RequestController.php

--- app/Http/Controllers/Admin/RequestController.php
+++ app/Http/Controllers/Admin/RequestController.php
@@ -3,9 +3,11 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
-use App\Imports\ProductImport;
+use App\Imports\CustomProductsImport;
+use App\Imports\InventoryImport;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 use Maatwebsite\Excel\Facades\Excel;
 
 class RequestController extends Controller
@@ -17,9 +19,26 @@ class RequestController extends Controller
      */
     public function importProductsBasics(Request $request): RedirectResponse
     {
-        //$file = $request->file('csv-import');
-        $path = storage_path() . '/app/csv-import/import.csv';
-        Excel::queueImport(new ProductImport, $path);
-        return redirect()->back()->with('success', $path);
+        $file = $request->file('csv-import');
+
+        Storage::disk('local')->putFileAs('/files/', $file, 'inventory.csv');
+
+        Excel::queueImport(new InventoryImport, storage_path().'/app/files/inventory.csv');
+        return redirect()->back()->with('success', 'The Job started successfully!');
+    }
+
+    /**
+     * Imports basic products
+     * @param Request $request
+     * @return RedirectResponse
+     */
+    public function importProductsCustom(Request $request): RedirectResponse
+    {
+        $file = $request->file('csv-import');
+
+        Storage::disk('local')->putFileAs('/files/', $file, 'productsCustom.csv');
+
+        Excel::queueImport(new CustomProductsImport, storage_path().'/app/files/productsCustom.csv');
+        return redirect()->back()->with('success', 'The Job started successfully!');
     }
 }
