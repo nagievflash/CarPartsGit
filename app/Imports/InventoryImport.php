9cba6d93d77fc5d0188468bbc7d013c58c05f3f5
Tarlan Nagiev <tarlan.nagiev@mail.ru>
2022-08-19 12:15:49 +0300

    Added Inventory Import Class

---
+++ app/Imports/InventoryImport.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Imports;
+
+use App\Models\Product;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Database\Eloquent\Model;
+use Maatwebsite\Excel\Concerns\RemembersRowNumber;
+use Maatwebsite\Excel\Concerns\ToModel;
+use Maatwebsite\Excel\Concerns\WithBatchInserts;
+use Maatwebsite\Excel\Concerns\WithChunkReading;
+use Maatwebsite\Excel\Concerns\WithHeadingRow;
+use Maatwebsite\Excel\Concerns\WithStartRow;
+use Maatwebsite\Excel\Concerns\WithUpserts;
+
+class InventoryImport implements ToModel, WithHeadingRow, WithChunkReading, WithBatchInserts, WithUpserts, WithStartRow, ShouldQueue
+{
+    use RemembersRowNumber;
+
+    public function startRow(): int
+    {
+        return 2;
+    }
+
+    public function getCsvSettings(): array
+    {
+        return [
+            'delimiter' => "\t"
+        ];
+    }
+
+    /**
+     * @param array $row
+     *
+     * @return Model|Product|null
+     */
+    public function model(array $row)
+    {
+        $product = Product::where('sku', $row['sku']);
+        if ($product->exists()) {
+            $product = $product->first();
+            $price = (float)$row['cost'] + (float)$row['shipping_cost'] + (float)$row['handling_cost'];
+            $qty = $row['stock_total'];
+            $product->price = $price;
+            $product->qty = $qty;
+            $product->save();
+        }
+    }
+
+    public function chunkSize(): int
+    {
+        return 500;
+    }
+
+    public function batchSize(): int
+    {
+        return 500;
+    }
+
+    public function uniqueBy()
+    {
+        return 'sku';
+    }
+}
