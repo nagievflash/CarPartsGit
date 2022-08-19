95643768de66ca8e750867a896d20b6bee98e63e
Tarlan Nagiev <tarlan.nagiev@mail.ru>
2022-08-19 12:15:34 +0300

    Changed importCSVItem view

--- resources/views/admin/importCSVItem.blade.php
+++ resources/views/admin/importCSVItem.blade.php
@@ -2,13 +2,13 @@
     <x-slot name="header">
         <div class="row">
             <div class="col-12 col-md-6 order-md-1 order-last">
-                <h3>Import products from CSV</h3>
-                <p class="text-subtitle text-muted">This is the products import page.</p>
+                <h3>Import inventory from csv/txt files</h3>
+                <p class="text-subtitle text-muted">This is the Inventory import page.</p>
             </div>
             <div class="col-12 col-md-6 order-md-2 order-first">
                 <nav aria-label="breadcrumb" class="breadcrumb-header float-start float-lg-end">
                     <ol class="breadcrumb">
-                        <li class="breadcrumb-item active" aria-current="page">CSV import</li>
+                        <li class="breadcrumb-item active" aria-current="page">csv/txt import</li>
                     </ol>
                 </nav>
             </div>
@@ -26,10 +26,10 @@
                     @csrf
                     <x-maz-input :id="'csv-import'"
                                  :name="'csv-import'"
-                                 :label="'Download correct CSV File with products'"
+                                 :label="'Download correct txt or csv inventory pricing file'"
                                  :type="'file'">
                     </x-maz-input>
-                    <a href="https://auth.ebay.com/oauth2/authorize?client_id=fastdeal-autoelem-PRD-4f2fb35bc-cbb0b166&response_type=code&redirect_uri=fastdeal24-fastdeal-autoel-ymxyoese&scope=https://api.ebay.com/oauth/api_scope https://api.ebay.com/oauth/api_scope/sell.marketing.readonly https://api.ebay.com/oauth/api_scope/sell.marketing https://api.ebay.com/oauth/api_scope/sell.inventory.readonly https://api.ebay.com/oauth/api_scope/sell.inventory https://api.ebay.com/oauth/api_scope/sell.account.readonly https://api.ebay.com/oauth/api_scope/sell.account https://api.ebay.com/oauth/api_scope/sell.fulfillment.readonly https://api.ebay.com/oauth/api_scope/sell.fulfillment https://api.ebay.com/oauth/api_scope/sell.analytics.readonly https://api.ebay.com/oauth/api_scope/sell.finances https://api.ebay.com/oauth/api_scope/sell.payment.dispute https://api.ebay.com/oauth/api_scope/commerce.identity.readonly https://api.ebay.com/oauth/api_scope/commerce.notification.subscription https://api.ebay.com/oauth/api_scope/commerce.notification.subscription.readonly">Get OAuth Access Token</a>
+
                     @if (session('success'))
                         <div class="alert alert-success alert-dismissible fade show" role="alert">
                             {{ session('success') }}
@@ -42,8 +42,8 @@
                     <div class="form-field mt-3">
                         <input type="text" class="form-control" name="query" placeholder="Название категории">
                     </div>--}}
-                    <div class="form-field mt-3">
-                        <button type="submit" class="btn btn-success form-submit">Отправить запрос</button>
+                    <div class="form-field mt-4">
+                        <button type="submit" class="btn btn-success form-submit">Send inventory</button>
                     </div>
                 </form>
             </div>
