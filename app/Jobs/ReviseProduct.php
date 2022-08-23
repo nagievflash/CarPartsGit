5a97b92dded371d940aeb003ebedb9b59b81562b
Tarlan Nagiev <tarlan.nagiev@mail.ru>
2022-08-23 20:22:25 +0500

    Removed useless classes from ReviseProduct Job

--- app/Jobs/ReviseProduct.php
+++ app/Jobs/ReviseProduct.php
@@ -5,7 +5,6 @@ namespace App\Jobs;
 use App\Helpers\EbayUploadHelper;
 use App\Models\EbayListing;
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldBeUnique;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
