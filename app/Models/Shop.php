5c341e60e20ce3c5f5eac65d1eddb9fde2a338bd
Tarlan Nagiev <tarlan.nagiev@mail.ru>
2022-08-29 16:00:28 +0500

    Added PHPDoc to Shops Model

--- app/Models/Shop.php
+++ app/Models/Shop.php
@@ -5,6 +5,25 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property integer $id
+ * @property string title
+ * @property string slug
+ * @property string email
+ * @property string store_url
+ * @property string token
+ * @property float percent
+ * @property integer min_qty
+ * @property integer qty_reserve
+ * @property string shipping_profile_id
+ * @property string shipping_profile_name
+ * @property string return_profile_id
+ * @property string return_profile_name
+ * @property string payment_profile_id
+ * @property string payment_profile_name
+ * @method static create(array $array)
+ * @method static where(string $string, mixed $input)
+ */
 class Shop extends Model
 {
     use HasFactory;
